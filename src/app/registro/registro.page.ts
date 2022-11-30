e046df4dd52a99c5a36b70b05f10d58791f2d2f2
BiahMarqyes <99971873+BiahMarqyes@users.noreply.github.com>
2022-11-30 11:39:43 -0300

    aula5-13min

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from '../models/Usuario';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-registro',
@@ -9,8 +12,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroPage implements OnInit {
 
   formRegistro: FormGroup;
+  usuario: Usuario = new Usuario();
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
     this.formRegistro = this.formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf:['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
@@ -23,8 +27,17 @@ export class RegistroPage implements OnInit {
   ngOnInit() {
   }
 
-  salvarRegistro() {
-    console.log('Formulário: ', this.formRegistro.valid);
+  async salvarRegistro() {
+    if(this.formRegistro.valid){
+      this.usuario.nome = this.formRegistro.value.nome;
+      this.usuario.cpf = this.formRegistro.value.cpf;
+      this.usuario.email = this.formRegistro.value.email;
+      this.usuario.senha = this.formRegistro.value.senha;
+      await this.storageService.set(this.usuario.email, this.usuario);
+      this.route.navigateByUrl('/tab1');
+    }else{
+      alert('Formulário Inválido!');
+    }
   }
 
 }
