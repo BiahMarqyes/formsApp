60bd5b46a792daf018f13f872d7e064082847e47
BiahMarqyes <99971873+BiahMarqyes@users.noreply.github.com>
2022-12-02 08:58:35 -0300

    2-12

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Usuario } from '../models/Usuario';
-import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +7,7 @@ import { StorageService } from '../services/storage.service';
 })
 export class LoginPage implements OnInit {
 
-
-  formLogin: FormGroup;
-  usuario: Usuario = new Usuario();
-
-  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
-    this.formLogin = this.formBuilder.group({
-      email:['', Validators.compose([Validators.required, Validators.email])],
-      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
-    });
-   }
+  constructor() { }
 
   ngOnInit() {
   }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -60,7 +60,7 @@ export class RegistroPage implements OnInit {
       this.usuario.email = this.formRegistro.value.email;
       this.usuario.senha = this.formRegistro.value.senha;
       await this.storageService.set(this.usuario.email, this.usuario);
-      this.route.navigateByUrl('/tabs/tab1/login');
+      this.route.navigateByUrl('/tabs/tab1');
     }else{
       alert('Formulário Inválido!');
     }
